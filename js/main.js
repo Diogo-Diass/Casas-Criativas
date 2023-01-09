4c18aa74aea0a0cabdf66104f4247f1ff764312a
diogo <diihrocha143@gmail.com>
2023-01-09 18:15:58 -0300

    voltar ao topo

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const botaoTopo = document.getElementById("topo")
+
+botaoTopo.addEventListener("click", function(){
+    window.scrollTo(0,0)
+})
+
+document.addEventListener("scroll", ocultar)
+
+function ocultar(){
+
+    if(window.scrollY > 10){
+
+        botaoTopo.style.display = "flex"
+    
+    }else{
+
+        botaoTopo.style.display = "none"
+
+    }
+
+ 
+} 
+ocultar()
